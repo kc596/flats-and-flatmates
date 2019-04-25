63a2ed6201b15035e213d81aefaa37173a4af6cf
kc596 <chaudhary.kc.kunal@gmail.com>
2019-04-25 22:39:11 +0530

    Developed efficient single threaded crawler

--- main.py
+++ main.py
@@ -1,46 +1,53 @@
 from datetime import datetime
-from queue import Queue
-from src.crawler import Crawler
+from src import fbcrawlerutils as fbutil
+from src.fbgroupcrawler import GroupCrawler
 from src.outputgenerator import OutputGenerator
-from src.utils import loadConfiguration
-import time
+from src.utils import loadConfiguration, getLogger
+from src.webdriver import WebDriver
 
 config = loadConfiguration("config/config.yaml")
-jobQueue = Queue()
-crawlers = []
+logger = getLogger("main")
 
-for i in range(config['input']['thread']):
-    crawler = Crawler(jobQueue)
-    crawler.start()
-    crawlers.append(crawler)
 
-while True:
-    startTime = int(datetime.now().timestamp())
-    print("Starting crawling at {}".format(datetime.now()))
-    for group in config['input']['groups']:
-        jobQueue.put(group)
-
-    if len(crawlers) > len(config['input']['groups']):
-        for i in range(len(crawlers)-len(config['input']['groups'])):
-            jobQueue.put("dummy")
-
-    # wait for completion of all jobs in queue
+def crawlTillEndOfTime():
     while True:
-        terminate = True
-        for retry in range(5):
-            for crawler in crawlers:
-                terminate = terminate and crawler.idle
-            time.sleep(60)
-        if terminate:
-            break
+        startTime = int(datetime.now().timestamp())
+        crawlAllGroupsOnce()
+        writeOutput(startTime)
 
-    print("Job completed! Generating output.")
-    outputFileName = config['output']['outputfileprefix']+"_"+datetime.now().strftime("%d_%m_%Y_%I_%M_%p")+".html"
+
+def crawlAllGroupsOnce():
+    for group in config['input']['groups']:
+        GroupCrawler(driver, group).crawlGroup()
+
+
+def writeOutput(startTime):
+    logger.info("Job completed! Generating output.")
+    outputFileName = config['output']['outputfileprefix'] + "_" + datetime.now().strftime("%d_%m_%Y_%I_%M_%p") + ".html"
     outputGenerator = OutputGenerator(outputFileName)
     outputGenerator.generateOuputFromDatabase()
-    print("Complete output written to file: {}".format(outputFileName))
-
-    newPostsFileName = config['output']['newpostsfileprefix']+"_"+datetime.now().strftime("%d_%m_%Y_%I_%M_%p")+".html"
+    logger.info("Complete output written to file: {}".format(outputFileName))
+    newPostsFileName = config['output']['newpostsfileprefix'] + "_" + datetime.now().strftime(
+        "%d_%m_%Y_%I_%M_%p") + ".html"
     newPostsOutputGenerator = OutputGenerator(newPostsFileName)
-    newPostsOutputGenerator.generateOuputFromDatabase(selectQuery="SELECT * FROM posts where timestamp>{}".format(startTime))
-    print("New posts written to file: {}".format(newPostsFileName))
+    newPostsOutputGenerator.generateOuputFromDatabase(
+        selectQuery="SELECT * FROM posts where timestamp>{}".format(startTime))
+    logger.info("New posts written to file: {}".format(newPostsFileName))
+
+
+if __name__=='__main__':
+    driver = WebDriver(config['webdriver']['chromeoptions']).getDriver()
+    fbutil.login(driver)
+    try:
+        crawlTillEndOfTime()
+    except Exception as e:
+        logger.exception(repr(e))
+
+'''
+def synchronized(func):
+    func.__lock__ = threading.Lock()
+    def synced_func(*args, **kws):
+        with func.__lock__:
+            return func(*args, **kws)
+    return synced_func
+'''
